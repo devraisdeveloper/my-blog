c0029521d5dce07113213506cba2249109572fba
devrais <devrais1@gmail.com>
2017-07-11 16:58:16 +0300

    Change in db.php

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=blog',
-    'username' => 'user1',
-    'password' => 'DevraisUser1',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=YourdatabaseName',
+    'username' => 'YourUserName',
+    'password' => 'YourDatabaseName',
     'charset' => 'utf8',
 ];
